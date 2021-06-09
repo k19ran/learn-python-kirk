18b806e0e2ad260f1e99922368c8af34d6745bea
Kiran Gunana <kiran.gunana@colruytgroup.com>
2021-06-09 19:49:56 +0200

    strings

---
+++ week1/py_strings_part2.py
@@ -0,0 +1,20 @@
+ip_addr = '192.168.1.1'
+print(ip_addr)
+print(ip_addr == '192.168.1.1')
+print(ip_addr == "192.168.100.1")
+print('192.168' in ip_addr )
+print('1.15' in ip_addr )
+print('1.15' not in ip_addr)
+print(ip_addr[0])
+print(ip_addr[-1])
+print(len(ip_addr))
+my_str = 'hello'
+print(my_str + ' something')
+print(my_str)
+my_str = my_str + ' something'
+print(my_str)
+#my_str = my_str + 1
+print(type((bin(255))))
+my_var = bin(255)
+print(my_var)
+print(int(my_var, 2))
