9db500d382cf63be7fee637feb7699b85e54a1e8
Kiran Gunana <kiran.gunana@colruytgroup.com>
2021-06-13 12:18:35 +0200

    week2

---
+++ week2/Numbers.py
@@ -0,0 +1,26 @@
+num1 = 5
+num2 = 7
+num3 = 5
+num4 = 2.56
+num5 = 3.1254789
+num6 = 0.1
+num7 = 0.2
+i=1
+
+print(num1+num2)
+print(num1-num2)
+print(type((num2/num1)))
+#below is the python2 way of division
+print(num2//num1)
+print(2**num3)
+print(num1 % num3)
+print(type(num1))
+print(int(num4))
+print(float(num4))
+print(round(num4))
+print(round(num5,3))
+print(num6+num7)
+i=i+1
+print(i)
+i+=1
+print(i)
