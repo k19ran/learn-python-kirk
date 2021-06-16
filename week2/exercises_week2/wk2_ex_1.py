d2beaa5ca13c8efe46c67751376d1aee55b39536
Kiran Gunana <kiran.gunana@colruytgroup.com>
2021-06-16 20:45:18 +0200

    lists

---
+++ week2/exercises_week2/wk2_ex_1.py
@@ -0,0 +1,28 @@
+'''
+Open the "show_version.txt" file for reading. Use the .read() method to read in the entire file contents to a variable. 
+Print out the file contents to the screen. Also print out the type of the variable (you should have a string at this point).
+
+Close the file.
+
+Open the file a second time using a Python context manager (with statement). Read in the file contents using the .readlines() method.
+Print out the file contents to the screen. Also print out the type of the variable (you should have a list at this point).
+'''
+banner = "-" * 80
+
+f = open("C:\learn-automation\learn-python-kirk\week2\show_version.txt")
+output = f.read()
+print(banner)
+print(output)
+print(banner)
+print("Type of variadble output : {}".format(type(output)))
+print(banner)
+f.close()
+
+with open("C:\learn-automation\learn-python-kirk\week2\show_version.txt") as f:
+    output= f.readlines()
+print(banner)
+print(output)
+print(banner)
+print("Type of variadble output : {}".format(type(output)))
+print(banner)
+f.close()
--- week2/exercises_week2/wk2_ex_2.py
+++ week2/exercises_week2/wk2_ex_2.py
@@ -12,3 +12,25 @@ Using the .pop() method to remove the first IP address in the list and the last 
 
 Update the new first IP address in the list to be '2.2.2.2'. Print out the new first IP address in the list.
 '''
+
+banner = "-" * 80
+
+ip_list = ["11.11.11.11","12.12.12.12","13.13.13.13","14.14.14.14","15.15.15.15"]
+print("First 5 ip addresses:{}".format(ip_list))
+ip_list.append("16.16.16.16")
+print("6 ip addresses:{}".format(ip_list))
+ip_list.extend(["17.17.17.17","18.18.18.18"])
+print("8 ip addresses:{}".format(ip_list))
+
+new_ip_list = ip_list + ["19.19.19.19","20.20.20.20"]
+print("New complete address list: {}".format(new_ip_list))
+print("First address from the list: {}".format(new_ip_list[0]))
+print("last address from the list: {}".format(new_ip_list[-1]))
+new_ip_list.pop(0) # first address pop
+new_ip_list.pop()# last adres pop
+print("New list after pop:{}".format(new_ip_list))
+new_ip_list[0] = "2.2.2.2"
+print("New list after assiging 2.2.2.2:{}".format(new_ip_list))
+print("First address in new list:{}".format(new_ip_list[0]))
+
+
