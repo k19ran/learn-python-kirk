9fdf82e6954bedb3cc10b8d8a0eacbf222c81b4b
Kiran Gunana <kiran.gunana@colruytgroup.com>
2021-06-19 17:03:04 +0200

    exe week3

--- week3/exercises_week3/wk3_ex_2.py
+++ week3/exercises_week3/wk3_ex_2.py
@@ -7,3 +7,36 @@ Using a conditional statement, also search for '10.220.88.30'. If this IP addres
 
 Keep track of whether you have found both the Default Gateway and the Arista3 switch. Once you have found both of these devices, 'break' out of the for loop.
 '''
+from pprint import pprint
+
+banner = "-"*40
+
+with open("C:\learn-automation\learn-python-kirk\week3\show_arp.txt",mode="r") as f:
+    mylist = f.readlines()
+
+new_list = []
+fields =[]
+
+found1,found2 = (False,False)
+
+for list in mylist:
+    if "Protocol" in list:
+        continue
+    new_list = list.split()
+    fields = new_list[1],new_list[3]
+    ip_addr = new_list[1]
+    mac_addr = new_list[3]
+    if fields[0] == "10.220.88.1":
+        print("Default gateway IP/Mac:{} {}".format(ip_addr,mac_addr))
+        found1 = True
+    elif fields[0] == "10.220.88.30":
+        print("Arista3 IP/Mac is:{} {}".format(ip_addr,mac_addr))
+        found2 = True
+    if found1 and found2:
+        print("Exsiting")
+        break
+
+    
+
+
+
--- week3/exercises_week3/wk3_ex_3.py
+++ week3/exercises_week3/wk3_ex_3.py
@@ -5,3 +5,29 @@ Keep reading the lines until you have encountered the remote "System Name" and r
 Save these two items into variables and print them to the screen. 
 You should extract only the system name and port id from the lines (i.e. your variables should only have 'twb-sf-hpsw1' and '15'). Break out of your loop once you have retrieved these two items.
 '''
+
+from pprint import pprint
+
+banner = "-"*40
+system_name,port_id = (None,None)
+
+with open("C:\learn-automation\learn-python-kirk\week3\show_lldp_neighbors_detail.txt",mode="r") as f:
+    lldp = f.readlines()
+
+fields = []
+
+for list in lldp:
+    if "system name" in list.lower():
+        system_name = list.split("System Name: ")
+    elif "port id" in list.lower():
+        port_id = list.split("Port id: ")
+    if system_name and port_id:
+        break
+#print(system_name[1],port_id[1])
+
+print("System Name:{}".format(system_name[1]))
+print("Port id:{}".format(port_id[1]))
+
+
+
+print(banner)
